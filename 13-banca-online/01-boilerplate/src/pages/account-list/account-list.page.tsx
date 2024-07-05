db79d1c38dbb9e9caad4ef4b0a2e12191d0fe865
Patricia Sánchez Nadales <patriciasancheznadales95@gmail.com>
2024-07-05 20:10:03 +0200

    mis cuentas window

--- 13-banca-online/01-boilerplate/src/pages/account-list/account-list.page.tsx
+++ 13-banca-online/01-boilerplate/src/pages/account-list/account-list.page.tsx
@@ -1,29 +1,28 @@
-import { appRoutes } from "@/core/router";
 import { AppLayout } from "@/layouts";
 import React from "react";
-import { generatePath, Link } from "react-router-dom";
+import { AccountVm } from "./account-list.vm";
+import classes from "./account-list.page.module.css";
+import { AccountListTableComponent } from "./components";
+import { getAccountList } from "./api";
+import { mapAccountListFromApiToVm } from "./account-list.mapper";
 
 export const AccountListPage: React.FC = () => {
+  const [accountList, setAccountList] = React.useState<AccountVm[]>([]);
+
+  React.useEffect(() => {
+    getAccountList().then((result) =>
+      setAccountList(mapAccountListFromApiToVm(result))
+    );
+  }, []);
+
   return (
     <AppLayout>
-      <div>
-        Account List
-        <br />
-        <Link to={generatePath(appRoutes.movements, { id: 1 })}>
-          Movimientos de cuenta 1
-        </Link>
-        <br />
-        <Link to={appRoutes.transfer}>Transferencia</Link>
-        <br />
-        <Link to={generatePath(appRoutes.transferFromAccount, { id: 1 })}>
-          Transferencia desde cuenta 1
-        </Link>
-        <br />
-        <Link to={appRoutes.createAccount}>Crear cuenta</Link>
-        <br />
-        <Link to={generatePath(appRoutes.editAccount, { id: 1 })}>
-          Editar cuenta 1
-        </Link>
+      <div className={classes.root}>
+        <div className={classes.headerContainer}>
+          <h1>Mis cuentas</h1>
+          <button>AGREGAR NUEVA CUENTA</button>
+        </div>
+        <AccountListTableComponent accountList={accountList} />
       </div>
     </AppLayout>
   );
