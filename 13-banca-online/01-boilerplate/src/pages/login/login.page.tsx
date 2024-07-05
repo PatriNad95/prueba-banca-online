aa33bc30575c90aaab2fc6b24c917767f1e66db6
Patricia Sánchez Nadales <patriciasancheznadales95@gmail.com>
2024-07-05 14:36:17 +0200

    login development

---
+++ 13-banca-online/01-boilerplate/src/pages/login/login.page.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Credentials } from "./login.vm";
+import { LoginFormComponent } from "./components";
+import { appRoutes } from "@/core/router";
+import { isValidLogin } from "./api";
+import { mapCredentialsFromVmToApi } from "./login.mapper";
+import { useNavigate } from "react-router-dom";
+import classes from "./login.page.module.css";
+
+export const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (credentials: Credentials) => {
+    const apiCredentials = mapCredentialsFromVmToApi(credentials);
+    isValidLogin(apiCredentials).then((isValid) => {
+      if (isValid) {
+        navigate(appRoutes.accountList);
+      } else {
+        alert("Usuario o clave no correctas ppsst: admin@email.com / test ");
+      }
+    });
+  };
+
+  return (
+    <>
+      <header className={classes.header}>
+        <img className={classes.logo} src="assets/logo_header.svg" />
+      </header>
+      <div className={classes.bgImg}></div>
+      <div className={classes.box}>
+        <h1>Acceso</h1>
+        <LoginFormComponent onLogin={handleSubmit} />
+        <h4 className={classes.inputFooter}>
+          Está Usted en un <strong>sitio seguro</strong>
+        </h4>
+      </div>
+    </>
+  );
+};
